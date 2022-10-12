eca98ea79986f41942b96a00655fc498ca01dc3c
Vitaly Osipov <vital-nvl@mail.ru>
2022-10-12 19:37:09 +0300

    Final program

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ int size = Convert.ToInt32(Console.ReadLine());
 string[] newArray = new string[size];
 for (int i = 0; i < size; i++)
 {
-    Console.WriteLine($"Введите {i+1}-й элемент: ");
+    Console.WriteLine($"Введите {i + 1}-й элемент: ");
     string element = Convert.ToString(Console.ReadLine());
     newArray[i] = element;
 }
