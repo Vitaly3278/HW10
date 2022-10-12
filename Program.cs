2950aafe905aae12dd8f2860c8c268a1eae6d61b
Vitaly Osipov <vital-nvl@mail.ru>
2022-10-12 19:05:14 +0300

    Create new array

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, 
+// длина которых меньше либо равна 3 символа.
+
+Console.Write("Введите количество элементов массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+string[] newArray = new string[size];
+for (int i = 0; i < size; i++)
+{
+    Console.WriteLine($"Введите {i+1}-й элемент: ");
+    string element = Convert.ToString(Console.ReadLine());
+    newArray[i] = element;
+}
+
+string[] finalArray = new string[size];
+int count = 0;
+
+for (int j = 0; j < size; j++)
+{
+    if (newArray[j].Length <= 3)
+    {
+        finalArray[count] = newArray[j];
+        count++;
+    }
+}
+Console.WriteLine();
